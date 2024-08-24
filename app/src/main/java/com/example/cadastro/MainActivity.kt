d1f23c4129b831ebad55670327765ebb8d054cc4
biankyrou <bianca.carvalio@aluno.ifsp.edu.br>
2024-08-24 15:34:19 -0300

    feat: adicionando botao de salvar

--- app/src/main/java/com/example/cadastro/MainActivity.kt
+++ app/src/main/java/com/example/cadastro/MainActivity.kt
@@ -21,7 +21,6 @@ class MainActivity : AppCompatActivity() {
 
         val ufs = resources.getStringArray(R.array.uf_lista)
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, ufs)
-
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
         amb.ufSpinner.adapter = adapter
@@ -35,10 +34,18 @@ class MainActivity : AppCompatActivity() {
             amb.femininoRb.isChecked = false
             amb.cidadeEt.text.clear()
             amb.ufSpinner.setSelection(0)
-        }
+
         Toast.makeText(
             this@MainActivity, "Formulário limpo!",
             Toast.LENGTH_SHORT
         ).show()
+        }
+
+        amb.salvarBt.setOnClickListener{
+            var response_form = Form(amb.inputNameEt.text.toString(), amb.inputTelEt.text.toString(), amb.inputEmailEt.text.toString(),
+                amb.emailCb.isChecked, amb.sexoRadioGroup.toString(), amb.cidadeEt.text.toString(), amb.ufSpinner.toString())
+
+            Toast.makeText(this@MainActivity, "Formulário salvo com sucesso! $response_form", Toast.LENGTH_SHORT).show()
+        }
     }
 }
