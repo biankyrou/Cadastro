92696430361b62f173ed6059ae499ec7c01442c5
biankyrou <bianca.carvalio@aluno.ifsp.edu.br>
2024-08-24 15:03:02 -0300

    feat: implementando ViewBinding e adicionando Spinner com ArrayAdapter no MainActiviy

--- app/src/main/java/com/example/cadastro/MainActivity.kt
+++ app/src/main/java/com/example/cadastro/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.example.cadastro
 
+import android.app.Activity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cadastro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var amb: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        val spinnerUf: Spinner = findViewById(R.id.ufSpinner)
+        val ufs = resources.getStringArray(R.array.uf_lista)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, ufs)
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinnerUf.adapter = adapter
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        amb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(amb.root)
     }
 }
