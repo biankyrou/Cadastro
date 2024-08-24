dd2cf37e467a0d805522bd90c75535e3a959d17a
biankyrou <bianca.carvalio@aluno.ifsp.edu.br>
2024-08-24 15:09:32 -0300

    refactor: refatorando MainActivity - Spinner Reference

--- app/src/main/java/com/example/cadastro/MainActivity.kt
+++ app/src/main/java/com/example/cadastro/MainActivity.kt
@@ -13,17 +13,16 @@ import com.example.cadastro.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var amb: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        val spinnerUf: Spinner = findViewById(R.id.ufSpinner)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        amb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(amb.root)
+
         val ufs = resources.getStringArray(R.array.uf_lista)
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, ufs)
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
-        spinnerUf.adapter = adapter
-
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        amb = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(amb.root)
+        amb.ufSpinner.adapter = adapter
     }
 }
